727f9b96b14cf1e4b93bb3ad355594195b59ec3b
ajaykumar130 <148423440+ajaykumar130@users.noreply.github.com>
2026-02-20 18:07:46 +0530

    Add files via upload

---
+++ Week1/Week1Day4/Problem1StudentGradeEvaluator.js
@@ -0,0 +1,13 @@
+let marks = 40;
+if(marks>=75){
+    console.log("A grade");
+}
+else if(marks>=60){
+    console.log("B grade");
+}
+else if(marks>=35){
+    console.log("pass");
+}
+else{
+    console.log("fail");
+}
---
+++ Week1/Week1Day4/Problem4NumberAnalysisTool.js
@@ -0,0 +1,14 @@
+let number = 5;
+
+let num = number >= 0 ? "Positive" : "Negative";
+console.log("Number is: " + num);
+
+if (number % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
+
+for (let i = 1; i <= number; i++) {
+    console.log(i);
+}
