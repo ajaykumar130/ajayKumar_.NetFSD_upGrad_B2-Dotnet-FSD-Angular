3e4bc24e82962f8275884ca6a6cefbe9ba4ee4f0
ajaykumar130 <148423440+ajaykumar130@users.noreply.github.com>
2026-03-11 22:46:00 +0530

    Add files via upload

---
+++ Week4/Week4Day3/EmployeeBonusCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Demo
+{
+    internal class EmployeeBonusCalculator
+    {
+        static void Main()
+        {
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter Salary: ");
+            double salary = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter Experience: ");
+            int experience = Convert.ToInt32(Console.ReadLine());
+            double bonusPercent;
+            if (experience < 2)
+                bonusPercent = 0.05;
+            else if (experience <= 5)
+                bonusPercent = 0.10;
+            else
+                bonusPercent = 0.15;
+
+            double bonus = salary * bonusPercent;
+            double finalSalary = salary + bonus;
+
+            Console.WriteLine("\nEmployee: " + name);
+            Console.WriteLine("Bonus: " + bonus);
+            Console.WriteLine("Final Salary: " + finalSalary);
+        }
+    }
+}
