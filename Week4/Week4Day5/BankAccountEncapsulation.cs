d2a64f468a38d8520be3cde52a0f273c064e93e3
ajaykumar130 <148423440+ajaykumar130@users.noreply.github.com>
2026-03-16 11:44:53 +0530

    Add files via upload

---
+++ Week4/Week4Day5/BankAccountEncapsulation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace FunctionsDemo
+{
+    internal class BankAccountEncapsulation
+    {
+        private double balance;
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Balance");
+            }
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        static void Main(string[] args)
+        {
+            BankAccountEncapsulation account = new BankAccountEncapsulation();
+
+            account.Deposit(1000);
+            account.Withdraw(300);
+            Console.WriteLine("Current Balance = " + account.GetBalance());
+        }
+    }
+}
