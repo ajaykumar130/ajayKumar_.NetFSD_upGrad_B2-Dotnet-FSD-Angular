51015f1028b4f27f276a18bedb3307246b667d40
ajaykumar130 <148423440+ajaykumar130@users.noreply.github.com>
2026-04-01 17:07:29 +0530

    Add files via upload

---
+++ Week7/Week7/Day2/CalculatorApp/CalculatorApp/Controllers/CalculatorController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApp.Controllers
+{
+    [Route("calc")]
+    public class CalculatorController : Controller
+    {
+        [Route("/")]
+        [HttpGet("add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+
+            ViewData["Result"] = result;
+
+            return View();
+        }
+    }
+}
---
+++ Week7/Week7/Day2/FeedbackApp/FeedbackApp/Controllers/FeedbackController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FeedbackApp.Controllers
+{
+    [Route("feedback")]
+    public class FeedbackController : Controller
+    {
+        [Route("/")]
+        [HttpGet("submit")]
+        public IActionResult Submit()
+        {
+            return View();
+        }
+
+        [HttpPost("submit")]
+        public IActionResult Submit(string name, string comments, int rating)
+        {
+            string message;
+
+            if (rating >= 4)
+            {
+                message = "Thank You for your valuable feedback!";
+            }
+            else
+            {
+                message = "We will improve based on your feedback.";
+            }
+
+            ViewData["Message"] = message;
+            ViewData["UserName"] = name;
+
+            return View();
+        }
+    }
+}
